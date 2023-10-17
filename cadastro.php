bdf8dd0b62417503ba2189c1be6c1be8c83de19c
haroldocurti <haroldocurti@gmail.com>
2023-10-17 10:31:15 -0300

    aula 3

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,12 @@
+<?php
+
+require 'autoload.php';
+
+
+$usuario = new \App\Alura\Usuario($_POST['nome'], $_POST['senha']);
+$contato = new \App\Alura\Contato($_POST['email'], $_POST['endereco'], $_POST['cep']);
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -13,13 +22,13 @@
 <h1>Cadastro feito com sucesso.</h1>
 <p>Seguem os dados de sua conta:</p>
 <ul class="list-group">
-    <li class="list-group-item">Primeiro nome: </li>
-    <li class="list-group-item">Sobrenome: </li>
-    <li class="list-group-item">Usuário: </li>
-    <li class="list-group-item">Senha: </li>
-    <li class="list-group-item">Telefone: </li>
-    <li class="list-group-item">Email: </li>
-    <li class="list-group-item">Endereço: </li>
+    <li class="list-group-item">Primeiro nome: <?php echo $usuario->getNome(); ?></li class="list-group-item">
+    <li class="list-group-item">Sobrenome: <?php echo $usuario->getSobrenome(); ?></li class="list-group-item">
+    <li class="list-group-item">Usuário: <?php echo $contato->getUsuario(); ?></li class="list-group-item">
+    <li class="list-group-item">Senha: <?php echo $usuario->getSenha(); ?></li class="list-group-item">
+    <li class="list-group-item">Telefone: </li class="list-group-item">
+    <li class="list-group-item">Email: <?php echo $contato->getEmail(); ?></li class="list-group-item">
+    <li class="list-group-item">Endereço: <?php echo $contato->getEnderecoCep(); ?></li class="list-group-item">
 </ul>
 </div>
 </body>
